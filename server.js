c922f8105901034ff6a46aee63c4f37067e53f36
Duniaalkilany <duniaalkilany908@gmail.com>
2021-07-09 23:37:26 +0300

    setup

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5001;
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to PICKNIC back end!');
+    res.send('Welcome to MovieHolic back end!');
   });
   
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}!`));
