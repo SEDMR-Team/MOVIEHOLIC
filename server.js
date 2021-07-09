4456a1a1dd0b052e98f9056edb490c6f90b0cd18
Duniaalkilany <duniaalkilany908@gmail.com>
2021-07-09 23:32:52 +0300

    set-up back-end

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to PICKNIC back end!');
+  });
+  
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}!`));
